19ebd1ac2ddda0950306e4912e5e83e8a340caa6
pje8740 <pje8740@cu.ac.kr>
2025-11-17 01:15:48 +0900

    feat: Detail페이지 구현(#9)
    - Detail 페이지 구현
    - 영화 상세 정보 query 이용하여 API 요청
    - 로딩과 에러 처리

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,3 +1,60 @@
+import { fetchMovieDetail } from "@api";
+import { TMDB_IMAGE_URL } from "@constants";
+import useFetch from "@hooks/useFetch";
+import { useParams } from "react-router";
+
 export const Detail = () => {
-  return <div>Detail</div>;
+  const { movieId } = useParams();
+
+  const { data, error, isLoading } = useFetch({
+    query: (options) => fetchMovieDetail({ movieId, ...options }),
+    queryKey: [movieId],
+  });
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  const {
+    backdropPath,
+    genres = [],
+    overview,
+    title,
+    voteAverage,
+  } = data || {};
+
+  return (
+    <main>
+      <div>
+        <div>
+          <img alt={title} src={TMDB_IMAGE_URL + backdropPath} />
+        </div>
+        <article>
+          <header>
+            <div>
+              <p>Movie Detail</p>
+              <h1>{title}</h1>
+            </div>
+            <div>
+              <span>
+                {Number.isFinite(voteAverage) ? voteAverage.toFixed(1) : "N/A"}
+              </span>
+              <ul>
+                {genres.map((genre) => (
+                  <li key={genre.id ?? genre.name}>{genre.name}</li>
+                ))}
+              </ul>
+            </div>
+          </header>
+          <section>
+            <h2>줄거리</h2>
+            <p>{overview}</p>
+          </section>
+        </article>
+      </div>
+    </main>
+  );
 };
